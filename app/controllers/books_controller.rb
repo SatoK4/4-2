0c79937a61403570c6395075e2807edd460d0b15
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-07-11 10:03:10 +0000

    [Add] createアクションの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,23 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      # 後ほど遷移先を変更
+      redirect_to books_path
+    else
+      render :index
+    end
+  end
+
   def show
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
