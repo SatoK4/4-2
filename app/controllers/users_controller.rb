47b5affdeea7d3d51c9a01080e40fb5e862d0d19
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-07-16 13:23:03 +0000

    [Add] 他のユーザー情報編集画面に遷移できない機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit]
   def index
     @user = User.all
   end
@@ -10,7 +11,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -29,4 +29,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+
+  def is_matching_login_user
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+
 end
